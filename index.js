8b560666f870f6c00b4e8d55d39cf33086ad1647
Sebastian <sebimarru@hotmail.com>
2023-09-04 01:25:26 +0300

    ejs introduction

--- index.js
+++ index.js
@@ -4,37 +4,69 @@ const path = require('path');
 const root_dir = (__dirname);
 
 app = express();
-
+app.set("view engine", "ejs");
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
+var wrongful_entries = [];
 
+// app.get(
+//     "/", function(req, res) {
+//         res.sendFile(path.join(root_dir, "/index.html"));
+//     }
+// );
 app.get(
     "/", function(req, res) {
-        res.sendFile(path.join(root_dir, "/index.html"));
+        res.render(path.join(root_dir, "/views/pagini/index.ejs"));
     }
 );
 app.get(
-    "/index.html", function(req, res) {
-        res.sendFile(path.join(root_dir, "/index.html"));
+    "/index", function(req, res) {
+        // res.sendFile(path.join(root_dir, "/index.html"));
+        res.render(path.join(root_dir, "/views/pagini/index.ejs"));
     }
 );
 app.get(
-    "/about.html", function(req, res) {
+    "/about", function(req, res) {
         res.sendFile(path.join(root_dir, "/about.html"));
     }
 );
 app.get(
-    "/images.html", function(req, res) {
+    "/images", function(req, res) {
         res.sendFile(path.join(root_dir, "/images.html"));
+    }
+);
+app.get(
+    "/pagina", function(req, res, next) {
+        res.render(path.join(root_dir, "/views/pagini/pagina.ejs"), {a:10, b:20, c:"sir"});
+        // console.log("aici");
+        // res.send("response");
     }
 );
 
 app.get(
-    "/something", function(req, res, next) {
-        console.log("aici");
-        res.send("response");
+    "/test", function(req, res, next) {
+        res.render(path.join(root_dir, "/views/pagini/test.ejs"));
+    }
+);
+app.get(
+    "/*", function(req, res) {
+        let user_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+        current_date = new Date();
+        breach_databody = {
+            "user_ip" : user_ip,
+            "url" : req.url,
+            "date" : current_date
+        }
+        console.log(
+            ">>>>> Wrong Path Access Detection!", 
+            "\n\tLink: \t\t", req.url,
+            "\n\tUser_IP:\t", user_ip,
+            "\n\tData body: ", breach_databody
+        );
+        wrongful_entries.push(breach_databody);
+        res.render(path.join(root_dir, "/views/pagini/error.ejs"));
     }
 );
 
 app.listen(8088);
-console.log('Listening on port 8088');
+console.log('Listening on port 8088...\n');
